c7e91cac2b96fe6ced00cdea9686116f36c2b471
Your Name <prajapatisonu50@GMAIL.com>
2025-04-21 07:28:47 +0530

    Initial commit

---
+++ client/src/components/Tagline.js
@@ -0,0 +1,44 @@
+//import Ambulance from "./../media/homepage_ambulance.webp"
+import Ambulance from "./../media/amb.gif"
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion"
+
+function Tagline(){
+    const n = useNavigate()
+
+
+
+    return( 
+        <div className="homepage_tagline">
+            <div className="tagline_text">
+                <h1>First <br /><b>E-Ambulance app</b></h1>
+                <p>Emergency care, just a tap away</p>
+                <motion.button whileTap={{scale: 0.8}} onClick={()=>{
+                    n("/signup")
+
+                }} >Signup for free!</motion.button>
+            </div>
+            <div className="tagline_ambulance">
+                <motion.img  
+
+                initial = {{
+                    y: 200,
+                }}
+
+                transition = {{
+                    y: {duration: .6}
+                }}
+
+                animate={{
+                    x: 0,
+                    y: 0,
+                    scale: 1,
+                    rotate: 0,
+                  }}
+                src={Ambulance} alt="Ambulance  - Online Ambulance Booking App" />
+            </div>
+        </div>
+    )
+}
+
+export default Tagline;
